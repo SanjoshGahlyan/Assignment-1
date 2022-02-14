a2df91a8786b62187a70eb8ba48f05e8092a83af
SanjoshGahlyan <96727910+SanjoshGahlyan@users.noreply.github.com>
2022-02-15 00:11:54 +0530

    Add files via upload

---
+++ Assignment 3 Outputs.py
@@ -0,0 +1,91 @@
+Question 1 Output:
+Enter the string: I love to travel in my own car in India.
+The occurence of:
+"i" : 1
+"love" : 1
+"to" : 1
+"travel" : 1
+"in" : 2
+"my" : 1
+"own" : 1
+"car" : 1
+"india" : 1
+
+Question 2 Output:
+Day:30
+Month:2
+Year:2002
+The given month has only 28 days
+Please enter a valid date
+Day:30
+Month:2
+Year:2000
+The given month has only 29 days
+Please enter a valid date
+Day:29
+Month:2
+Year:2000
+Next Date is: 1/3/2000
+
+Question 3 Output:
+Enter the list of integers: [3,34,23,7]
+Output: [(3, 9), (34, 1156), (23, 529), (7, 49)]
+
+Question 4 Output:
+Enter the grade point of the student: 8
+Your Grade is 'B+' and Very Good Performance
+
+Question 5 Output:
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Question 6 Output:
+Enter the name of the student: Sanjosh
+Enter the SID of Sanjosh: 21104022
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: Mandeep
+Enter the SID of Mandeep: 21103120
+
+You have entered 2 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of Sanjosh is 21104022
+The SID of Mandeep is 21103120
+
+b. Student Details (sorted with respect to names):
+The SID of Mandeep is 21103120
+The SID of Sanjosh is 21104022
+
+c. Student Details (sorted with respect to SIDs):
+The SID of Mandeep is 21103120
+The SID of Sanjosh is 21104022
+
+d. Enter the SID of the student: 21105029
+The SID you entered isn't entered
+Please enter a valid SID to be searched
+List of valid SIDs: [21104022, 21103120]
+
+Enter the SID of the student: 21104022
+The name of student whose SID is 21104022 is Sanjosh
+
+Question 7 Output:
+Enter the no. of terms of the Fibonacci sequence: 7
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 
+The average of the terms of Fibonacci sequence upto 7 terms is 2.86
+
+Question 8 Output:
+a. Set of all elements that are in Set1 and Set2 but not in both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
+
+
+    
